93117c8e363147777d63354991017cd91b82bd9d
Mindtraveller <vladimir.penyazkov@gmail.com>
2019-04-08 22:49:24 +0300

    Update build.sh and bundle to include subfolders

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cat ./src/**/* | /Users/mindtraveller/.npm-packages/lib/node_modules/uglify-es/bin/uglifyjs -o ./docs/bundle.js --compress --mangle
+cat ./src/**/*.js ./src/**.js | /Users/mindtraveller/.npm-packages/lib/node_modules/uglify-es/bin/uglifyjs -o ./docs/bundle.js --compress --mangle
 uglifycss ./styles/** --output ./docs/styles.css
